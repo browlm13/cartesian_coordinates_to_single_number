779abe0c960dab0ec045c6fa08d2b2930b079c16
LJ Brown <browlm13@gmail.com>
2018-02-26 23:47:57 -0600

    Create archimedean_spiral.py

---
+++ archimedean_spiral.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import math
+#import numpy as np
+import sys
+
+"""
+	cartesean coordinates, time parameter conversion using Archimedean Spiral
+"""
+
+def archimedean_spiral_to_cartesean(t, a=sys.float_info.min): pass
+def archimedean_spiral_from_cartesean(cartesean_pair, a=sys.float_info.min): pass
